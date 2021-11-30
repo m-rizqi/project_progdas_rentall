bbfc689f158bb43fc665ae3c68af30f2a2ad7116
Muhammad Rizqi <mrizqi070502@gmail.com>
2021-11-30 10:05:12 +0700

    try using include

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,13 @@
 #include <iostream>
+#include "Person.h"
 using namespace std;
 
-int main(int argc, char *argv[])
+int main()
 {
 	std::cout << "Hello world!" << std::endl;
 	std::cout << "nanta nih bossss!" << std::endl;
 	std::cout << "test" << std::endl;
 	cout << "Anjay" << endl;
+	Person person;
+	person.sayHi();
 }
