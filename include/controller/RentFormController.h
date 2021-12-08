bdad52d42dad4ba3fa833d7bf738a09bee2474c5
Your Name <rhafif2018@gmail.com>
2021-12-08 16:18:13 +0700

    mancing mania mantap

--- include/controller/RentFormController.h
+++ include/controller/RentFormController.h
@@ -1,21 +1,16 @@
-/**
- * Project Untitled
- */
-
+#include "../../include/repository/RentRepository.h"
 
 #ifndef _RENTFORMCONTROLLER_H
 #define _RENTFORMCONTROLLER_H
 
 class RentFormController {
 public: 
-    
-void displayFormat();
-    
-void searchData();
-    
+
 void rentCar();
     
 void returnCar();
+private :
+RentRepository rentRepository;
 };
 
 #endif //_RENTFORMCONTROLLER_H
