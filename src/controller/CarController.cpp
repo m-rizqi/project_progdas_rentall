fc46d2f1d6a958c4dd8a16696622765e4ff7a05b
nantapranawa <nantapranawa@gmail.com>
2021-12-08 20:30:33 +0700

    Baru progress dikit wkwk

--- src/controller/CarController.cpp
+++ src/controller/CarController.cpp
@@ -152,24 +152,25 @@ void CarController::deleteCar(){
     Car car;        // ini?
 
     cout<< "-- D E L E T E  C A R --\n";
-    cout<< "Mobil akan dihapus berdasarkan nomor polisi\n";
+    cout<< "Mobil akan dihapus berdasarkan Id\n";
 
-    cout<< "Silahkan masukkan nomor polisi dari mobil yang ingin dihapus: \n";
-    long nomor_polisi;
-    cin>> nomor_polisi;
-    cin>> temp;
+    cout<< "Silahkan masukkan Id yang ingin dihapus: \n";
+    long id;
+    cin>> id;
+    getline(cin, temp);
 
-    car = carRepository.findData(nomor_polisi);     //ini find data apa
+    car = carRepository.findData(id);     //bener ga ya
 
-    if (car.getId() == 0)   // get apaaa
+    if (car.getId() == 0) 
     {
-        cout<< "Maaf, nomor polisi tidak ditemukan\n";
+        cout<< "Maaf, Id tidak ditemukan\n";
     }
     else{
         cout<< "-- Konfirmasi --\n";
         cout<< " Silahkan dicek kembali data mobil yang ingin dihapus\n";
         car.print();
 
+        cout<< "Apakah anda sudah yakin dan ingin menghapus?";
         cout<< "Tekan y untuk menghapus data...\n";
         cout<< "Tekan n untuk batal menghapus...\n";
         while(true){
@@ -204,6 +205,40 @@ void CarController::displayCars(int entry) // belum selesai gaess
     for(int i = idx; i < entry; i++){
      CarList.at(i).print();
     }
+    cout<<"===================================\n";
+    cout<< "%d car/page                page: 1\n", entry;
+    cout<< "\nOpsi lainnya : \n";
+    cout<< "1. previous page\n";
+    cout<< "2. next page\n";
+    cout<< "3. stop display\n";
+    
+    cout<< "Silahkan pilih opsi: ";     //ini perlu dicek sih
+    int option;
+    cin>> option;
+    getline(cin,temp);
+    switch (option)
+    {
+        case 1:
+        navigatePrevious(entry, idx, carList);
+        break;
 
+        case 2:
+        navigateNext(entry, idx, carList);
+        break;
+        
+        default:
+        break;
+    }
 
 }// close program
+
+/** 
+ * function to show the previous page of display cars
+ */
+        //belum selesai juga gaess maapp
+void CarController::navigatePrevious(int entry, int idx, vector<Car> carList)
+{
+    system("CLS");
+    string temp;
+    bool stop = false;
+}
