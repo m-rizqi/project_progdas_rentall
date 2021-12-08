c110441716dc84f7b3e62fd82e4ea87db3ff3225
BanaIsmail <bana2712@gmail.com>
2021-12-08 16:07:32 +0700

    Makasih Rizqi

--- src/controller/RentFormController.cpp
+++ src/controller/RentFormController.cpp
@@ -1,29 +1,92 @@
-/**
- * Project Untitled
- */
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <conio.h>
+#include <iostream>
+#include "../../include/controller/RentFormController.h"
+using namespace std;
 
-
-#include "RentFormController.h"
-
-/**
- * RentFormController implementation
- * 
- * Child class from Controller to handle about rent form
- */
-
-
-void RentFormController::displayFormat() {
-
+RentFormController::RentFormController()
+{
+    this->rentRepository = RentRepository();
 }
 
-void RentFormController::searchData() {
+void RentFormController::rentCar(){
+    long customerId, carId;
+    string destination, expRentDate, expreturn; 
+        cout <<  " Formulir Penyewaan SEWA.in ";
+        cout << " ================== ";
+        cout << " Silahkan Isi Formulir Di Bawah Ini";
+        cout << "Customer Id \t : ";
+        cin >> customerId;
+        cout << "Car Id \t : ";
+        cin >> carId;
+        cout << "Destinasi \t : ";
+        cin >> destination;
+        cout << "Tanggal Sewa \t : ";
+        cin >> expRentDate;
+        cout << "Lama Sewa \t : ";
+        cin >> expreturn;
 
-}
+        RentForm rentForm = RentForm (0, customerId, carId, destination, expRentDate, expreturn,"");
+        rentRepository. appendData (rentForm);
 
-void RentFormController::rentCar() {
+        cout<< "Data Anda Telah Kami Simpan";
 
 }
 
 void RentFormController::returnCar() {
+    long custId;
+    RentForm oldForm;
+    cout << "Formulin Pengembalian SEWA.in";
+    cout << "=============================";
+    cout << "Masukkan Customer Id Anda \t : ";
+    cin >> custId;
+    vector<RentForm> result = rentRepository.searchByCustomerId (custId);
+    if (result.size()==0){
+        oldForm = result.at(0);
+        long customerId, carId;
+        string destination, expRentDate, expreturn, retDate;
+        cout << "Data Anda \n";
+        oldForm.print();
+
+        cout << "Masukkan Tanggal Pengembalian";
+        cin >> retDate;
+        customerId = oldForm.getCustomerId();
+        carId = oldForm.getCarId();
+        destination = oldForm.getDestination();
+        expRentDate = oldForm.getRentalDate();
+        expreturn = oldForm.getExpectedReturnDate();
+
+        RentForm newForm = RentForm (0, custId, carId, destination, expRentDate, expreturn, retDate);
+
+        cout << "Konfirumasi Pengembalian Mobil Anda";
+        newForm.print();
+        cout << "Apakah Anda Yakin Akan Mengembalikan Mobil? (Tekan 'y' untuk ya, 'n' untuk tidak) ";
+         while (true)
+        {
+            char key = getch();
+            if (key =='y')
+            {
+                rentRepository.updateData(newForm);
+                printf("-- Terimakasih Telah Menggunakan SEWA.in--");
+                break;
+            }
+            else if (key == 'n')
+            {
+                printf("-- Pengembalian Mobil Batal --");
+                break;
+            }
+        }
+
+    }
+    else {
+        cout << "Customer Id Anda Tidak Ditemukan!";
+    
+    }
+    
+
+    
+    
 
 }
